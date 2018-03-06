b06abeccc7cc025b339e17dede06329eaf6d476d
Rain Wang <rainwzp@gmail.com>
2018-03-06 10:31:44 +0800

    更新模板

--- template/build/utils.js
+++ template/build/utils.js
@@ -1,28 +1,47 @@
-var path = require('path')
-var config = require('../config')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+'use strict'
+const path = require('path')
+const config = require('../config')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const pkg = require('../package.json')
+
+const glob = require('glob')
+const fs = require('fs')
+const _ = require('lodash')
+const routeMetaReg = /<routeMeta[^>]*>([\s\S]*?)<\/routeMeta>/
+
+exports.isProduction = function() {
+  return "development,testing,staging,production".indexOf(process.env.NODE_ENV) > -1
+}
 
 exports.assetsPath = function (_path) {
-  var assetsSubDirectory = process.env.NODE_ENV === 'production'
+  const assetsSubDirectory = exports.isProduction()
     ? config.build.assetsSubDirectory
     : config.dev.assetsSubDirectory
+
   return path.posix.join(assetsSubDirectory, _path)
 }
 
 exports.cssLoaders = function (options) {
   options = options || {}
 
-  var cssLoader = {
+  const cssLoader = {
     loader: 'css-loader',
     options: {
-      minimize: process.env.NODE_ENV === 'production',
+      sourceMap: options.sourceMap
+    }
+  }
+
+  const postcssLoader = {
+    loader: 'postcss-loader',
+    options: {
       sourceMap: options.sourceMap
     }
   }
 
   // generate loader string to be used with extract text plugin
   function generateLoaders(loader, loaderOptions) {
-    var loaders = [cssLoader]
+    const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
+
     if (loader) {
       loaders.push({
         loader: loader + '-loader',
@@ -58,77 +77,180 @@ exports.cssLoaders = function (options) {
 
 // Generate loaders for standalone style files (outside of .vue)
 exports.styleLoaders = function (options) {
-  var output = []
-  var loaders = exports.cssLoaders(options)
-  for (var extension in loaders) {
-    var loader = loaders[extension]
+  const output = []
+  const loaders = exports.cssLoaders(options)
+
+  for (const extension in loaders) {
+    const loader = loaders[extension]
     output.push({
       test: new RegExp('\\.' + extension + '$'),
       use: loader
     })
   }
+
   return output
 }
 
-// glob是webpack安装时依赖的一个第三方模块，还模块允许你使用 *等符号, 例如lib/*.js就是获取lib文件夹下的所有js后缀名的文件
-var glob = require('glob')
-// 页面模板
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-// 取得相应的页面路径，因为之前的配置，所以是src文件夹下的pages文件夹
-var PAGE_PATH = path.resolve(__dirname, '../src/pages')
-// 用于做相应的merge处理
-var merge = require('webpack-merge')
+exports.createNotifierCallback = () => {
+  const notifier = require('node-notifier')
 
+  return (severity, errors) => {
+    if (severity !== 'error') return
 
-//多入口配置
-// 通过glob模块读取pages文件夹下的所有对应文件夹下的js后缀文件，如果该文件存在
-// 那么就作为入口处理
-exports.entry = function () {
-  return entries(PAGE_PATH + '/*/*.js')
-}
+    const error = errors[0]
+    const filename = error.file && error.file.split('!').pop()
 
-function entries(globPath) {
-  var entries = {}
-  /**
-   * 读取src目录,并进行路径裁剪
-   */
-  glob.sync(globPath).forEach(function (entry) {
-    var tmp = entry.split('/').splice(-3)
-    var moduleName = tmp.slice(1, 2).toString().toLowerCase()
-    // ***************end***************
-    entries[moduleName] = entry
-  });
-  // 获取的主入口如下： { main: './src/pages/index/main.js', test: './src/pages/test/test.js' }
-  return entries
-}
-
-
-//多页面输出配置
-// 与上面的多页面入口配置相同，读取pages文件夹下的对应的html后缀文件，然后放入数组中
-exports.htmlPlugin = function () {
-  let entryHtml = entries(PAGE_PATH + '/*/*.html')
-  let arr = []
-  for (var fileName in entryHtml) {
-    let conf = {
-      // 模板来源
-      template: entryHtml[fileName],
-      // 文件名称
-      filename: fileName + '.html',
-      // 页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有的js脚本
-      chunks: ['manifest', 'vendor', fileName],
-      inject: true
-    }
-    if (process.env.NODE_ENV === 'production') {
-      conf = merge(conf, {
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeAttributeQuotes: true
-        },
-        chunksSortMode: 'dependency'
-      })
-    }
-    arr.push(new HtmlWebpackPlugin(conf))
+    notifier.notify({
+      title: pkg.name,
+      message: severity + ': ' + error.name,
+      subtitle: filename || '',
+      icon: path.join(__dirname, 'logo.png')
+    })
   }
-  return arr
+}
+
+exports.getEntry = function (globPath) {
+  var entries = {},
+    basename, tmp, pathname;
+
+  glob.sync(globPath).forEach(function (entry) {
+    basename = path.basename(entry, path.extname(entry));
+    tmp = entry.split('/').splice(-3);
+    pathname = tmp.splice(1, 1).toString().toLowerCase(); // 正确输出js和html的路径
+    entries[pathname] = entry;
+  });
+  console.log(entries)
+  return entries;
+}
+
+function cleanChildrenRoutes(routes, isChild = false) {
+  let start = -1
+  let routesIndex = []
+  routes.forEach(route => {
+    if (/-index$/.test(route.name) || route.name === 'index') {
+      // Save indexOf 'index' key in name
+      let res = route.name.split('-')
+      let s = res.indexOf('index')
+      start = start === -1 || s < start ? s : start
+      routesIndex.push(res)
+    }
+  })
+  routes.forEach(route => {
+    route.path = isChild ? route.path.replace('/', '') : route.path
+    if (route.path.indexOf('?') > -1) {
+      let names = route.name.split('-')
+      let paths = route.path.split('/')
+      if (!isChild) {
+        paths.shift()
+      } // clean first / for parents
+      routesIndex.forEach(r => {
+        let i = r.indexOf('index') - start //  children names
+        if (i < paths.length) {
+          for (let a = 0; a <= i; a++) {
+            if (a === i) {
+              paths[a] = paths[a].replace('?', '')
+            }
+            if (a < i && names[a] !== r[a]) {
+              break
+            }
+          }
+        }
+      })
+      route.path = (isChild ? '' : '/') + paths.join('/')
+    }
+    route.name = route.name.replace(/-index$/, '')
+    if (route.children) {
+      if (route.children.find(child => child.path === '')) {
+        delete route.name
+      }
+      route.children = cleanChildrenRoutes(route.children, true)
+    }
+  })
+  return routes
+}
+
+exports.createRoutes = function createRoutes(files, srcDir) {
+  const moduleName = srcDir.split(/[\\\/]/).slice(-1)
+
+  let routes = []
+  files.forEach(file => {
+    let keys = file
+      .replace(/^pages/, '')
+      .replace(/\.(vue|js)$/, '')
+      .replace(/\/{2,}/g, '/')
+      .split('/')
+      .slice(1)
+    // 读取file中的routeMeta段落
+    const content = fs.readFileSync(srcDir + "/" + file, 'utf-8')
+    let routeMeta = '{}'
+    try {
+      const results = content.match(routeMetaReg)
+      routeMeta = results[1]
+    } catch (e) {
+    }
+    let route = { name: '', path: '', component: '../' + file, meta: routeMeta}
+    let parent = routes
+    keys.forEach((key, i) => {
+      route.name = route.name
+        ? route.name + '-' + key.replace('_', '')
+        : key.replace('_', '')
+      route.name += key === '_' ? 'all' : ''
+      route.chunkName = moduleName + "_" + file.replace(/\.(vue|js)$/, '').replace(/\//g, '_')
+      let child = _.find(parent, { name: route.name })
+      if (child) {
+        child.children = child.children || []
+        parent = child.children
+        route.path = ''
+      } else {
+        if (key === 'index' && i + 1 === keys.length) {
+          route.path += i > 0 ? '' : '/'
+        } else {
+          route.path += '/' + (key === '_' ? '*' : key.replace('_', ':'))
+          if (key !== '_' && key.indexOf('_') !== -1) {
+            route.path += '?'
+          }
+        }
+      }
+    })
+    // Order Routes path
+    parent.push(route)
+
+    parent.sort((a, b) => {
+      if (!a.path.length) {
+        return -1
+      }
+      if (!b.path.length) {
+        return 1
+      }
+      // Order: /static, /index, /:dynamic
+      // Match exact route before index: /login before /index/_slug
+      if (a.path === '/') {
+        return /^\/(:|\*)/.test(b.path) ? -1 : 1
+      }
+      if (b.path === '/') {
+        return /^\/(:|\*)/.test(a.path) ? 1 : -1
+      }
+      let i = 0
+      let res = 0
+      let y = 0
+      let z = 0
+      const _a = a.path.split('/')
+      const _b = b.path.split('/')
+      for (i = 0; i < _a.length; i++) {
+        if (res !== 0) {
+          break
+        }
+        y = _a[i] === '*' ? 2 : _a[i].indexOf(':') > -1 ? 1 : 0
+        z = _b[i] === '*' ? 2 : _b[i].indexOf(':') > -1 ? 1 : 0
+        res = y - z
+        // If a.length >= b.length
+        if (i === _b.length - 1 && res === 0) {
+          // change order if * found
+          res = _a[i] === '*' ? -1 : 1
+        }
+      }
+      return res === 0 ? (_a[i - 1] === '*' && _b[i] ? 1 : -1) : res
+    })
+  })
+  return cleanChildrenRoutes(routes)
 }
--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -1,47 +1,58 @@
-var path = require('path')
-var utils = require('./utils')
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
 
-var projectRoot = path.resolve(__dirname, '../')
 const vuxLoader = require('vux-loader')
+const markdown = require('./webpack.markdown.js')
+const webpack = require('webpack')
 
-var config = require('../config')
-var vueLoaderConfig = require('./vue-loader.conf')
+const glob = require('glob')
+const fs = require('fs')
+const _ = require('lodash')
+const hash = require('hash-sum')
 
 function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
+{{#lint}}const createLintingRule = () => ({
+  test: /\.(js|vue)$/,
+  loader: 'eslint-loader',
+  enforce: 'pre',
+  include: [resolve('src'), resolve('test')],
+  options: {
+    formatter: require('eslint-friendly-formatter'),
+    emitWarning: !config.dev.showEslintErrorsInOverlay
+  }
+}){{/lint}}
+
+let entries = utils.getEntry('./src/modules/*/*.js'); // 获得入口js文件
+
 let webpackConfig = {
-  entry: utils.entry(),
+  entry: entries,
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath: utils.isProduction() ?
+      config.build.assetsPublicPath : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
+    modules: ['node_modules'],
     alias: {
-      '~vue': 'vue/dist/vue.esm.js',
-      '~src': resolve('src'),
-      '~components': resolve('src/components'),
-      '~css': resolve('static/css'),
-      '~img': resolve('static/img'),
-      '~js': resolve('static/js'),
+      {{#if_eq build "standalone"}}
+      'vue$': 'vue/dist/vue.esm.js',
+      {{/if_eq}}
+      '@': resolve('src')
     }
   },
   module: {
     rules: [
-      {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: [resolve('src'), resolve('test')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
+      {{#lint}}
+      ...(config.dev.useEslint ? [createLintingRule()] : []),
+      {{/lint}}
       {
         test: /\.vue$/,
         loader: 'vue-loader',
@@ -57,7 +68,15 @@ let webpackConfig = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
       {
@@ -65,14 +84,61 @@ let webpackConfig = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       },
+      {
+        test: /\.md$/,
+        loader: 'vue-markdown-loader',
+        options: markdown.getMarkDownSetting()
+      }
     ]
+  },
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
   }
 }
 
-
 module.exports = vuxLoader.merge(webpackConfig, {
-  plugins: ['vux-ui', 'progress-bar', 'duplicate-style']
+  plugins: [{
+    name: 'vux-ui'
+  }, {
+    name: 'progress-bar'
+  }, {
+    name: 'duplicate-style',
+  }, {
+    name: 'js-parser',
+    test: /src[\/\\]modules[\/\\]\w+[\/\\]router[\/\\]index\.js/,
+    fn: function (source) {
+      const srcDir = this.resourcePath.replace(/[\/\\]router[\/\\]index.js/, "")
+      const files = {}
+      glob.sync('pages/**/*.{vue,js}', {
+        cwd: srcDir
+      }).forEach(f => {
+        const key = f.replace(/\.(js|vue)$/, '')
+        if (/\.vue$/.test(f) || !files[key]) {
+          files[key] = f
+        }
+      })
+
+      let routes = utils.createRoutes(Object.values(files), srcDir)
+      // 读取路由模板
+      const route_template = fs.readFileSync('build/template/router.js', 'utf-8')
+      const template = _.template(route_template + '\n' + source)
+      return template({
+        'routes': routes,
+        'uniqBy': _.uniqBy,
+        'hash': hash
+      })
+    }
+  }]
 })
