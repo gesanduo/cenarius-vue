fa8b14d19fc0d9af9c63c26eafde1daf393eedf3
Rain Wang <rainwzp@gmail.com>
2018-03-26 14:34:00 +0800

    fix parse error

--- template/src/modules/demo_vant/pages/goods.vue
+++ template/src/modules/demo_vant/pages/goods.vue
@@ -9,7 +9,7 @@
     <van-cell-group>
       <van-cell>
         <div class="goods-title">{{goods.title}}</div>
-        <div class="goods-price">{{formatPrice(goods.price)}}</div>
+        <div class="goods-price">{{formatPrice()}}</div>
       </van-cell>
       <van-cell class="goods-express">
         <van-col span="10">运费：{{goods.express}}</van-col>
